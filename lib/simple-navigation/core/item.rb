86dd88fec630607b2d3622c21d8d2d53da513f03
Michael Hellein <themichaek@gmail.com>
2013-01-19 14:08:40 -0500

    Refactored simple-navigation patch to duplicate less code.

--- lib/simple-navigation/core/item.rb
+++ lib/simple-navigation/core/item.rb
@@ -1,8 +1,11 @@
 module SimpleNavigation
   class Item
     # We want simple-navigation to be forgetful about @selected.
-    def selected?
-      selected_by_config? || selected_by_subnav? || selected_by_condition?
+    def selected_with_forgetfulness?
+      is_selected = selected_without_forgetfulness?
+      @selected = nil
+      is_selected
     end
+    alias_method_chain :selected?, :forgetfulness
   end
 end
